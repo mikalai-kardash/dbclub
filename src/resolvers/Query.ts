e444054c11c92e34746119b4f3264b56d2133ae8
Mikalai Kardash <mikalai.kardash@gmail.com>
2018-09-16 22:07:04 -0700

    Added poc for query builder.

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,7 +1,48 @@
 import { getEmployee } from '../db/employee'
 import { Employee } from '../schema/models'
 
-const resolveEmployee = async (): Promise<Employee> => {
+const tableName = 'employees'
+const employeeFields = [
+    'id',
+    'firstName',
+    'lastName',
+]
+
+interface WhereInput {
+    AND?: WhereInput[]
+    OR?: WhereInput[]
+}
+
+interface Args {
+    where?: WhereInput
+}
+
+interface Query {
+    query: string
+    parameters: any[]
+}
+
+const buildQuery = (args: Args): string => {
+    let query = `SELECT * FROM ${tableName}`
+
+    if (args.where) {
+        const condition = employeeFields
+            .map(s => ({
+                field: s,
+                value: args.where[s],
+            }))
+            .find(f => f.value)
+
+        if (condition) {
+            query += ` WHERE ${condition.field} = ?`
+        }
+    }
+
+    return query
+}
+
+const resolveEmployee = async (_parent: any, _args: any): Promise<Employee> => {
+    console.log(buildQuery(_args))
     const employee = await getEmployee()
     return {
         id: employee.emp_no,
--- src/schema/models.ts
+++ src/schema/models.ts
@@ -3,3 +3,29 @@ export interface Employee {
     firstName: string
     lastName: string
 }
+
+export interface EmployeeWhereInput {
+    AND?: EmployeeWhereInput[]
+    OR?: EmployeeWhereInput[]
+
+    id?: number
+    firstName?: string
+    lastName?: string
+}
+
+export interface EmployeeOrderByInput {
+    id?: number
+    id_DESC?: number
+
+    firstName?: string
+    firstName_DESC?: string
+
+    lastName?: string
+    lastName_DESC?: string
+}
+
+export interface EmployeeArguments {
+    where?: EmployeeWhereInput
+    skip?: number
+    limit?: number
+}
